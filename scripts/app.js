a91bd136292f5286da0e036a07cd0ec8939bda2c
abhu-k <abhukode@gmail.com>
2025-04-27 14:21:49 +0530

    First edits for counter app

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+let Count = 0;
+const countDisplay = document.getElementById("count");
+const incrementbtn = document.getElementById("increment");
+const decrementbtn = document.getElementById("decrement");
+
+incrementbtn.onclick = () =>
+{
+    Count ++;
+    countDisplay.textContent = Count;
+}
+decrementbtn.onclick = () =>
+{
+    Count --;
+    countDisplay.textContent = Count;
+}
